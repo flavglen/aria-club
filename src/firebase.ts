32378a2e5f0209fc92933e8925bd687ea6afbb6e
flavglen <flavglen@gmail.com>
2023-10-25 16:53:40 -0400

    update env

---
+++ src/firebase.ts
@@ -0,0 +1,35 @@
+import { initializeApp } from "firebase/app";
+import { 
+  getFirestore, 
+  collection, 
+  getDocs, 
+  Firestore, 
+  addDoc,
+  doc, 
+  setDoc,
+  getDoc, 
+  updateDoc,
+  query,
+  orderBy,
+  limit,
+  serverTimestamp
+} from "firebase/firestore/lite";
+import { getStorage, ref, uploadBytes  } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "ariaclubindia.firebaseapp.com",
+  projectId: "ariaclubindia",
+  storageBucket: "ariaclubindia.appspot.com",
+  messagingSenderId: "481878676349",
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: "G-02FFMXYC6H"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db: Firestore = getFirestore(app)
+const imageDB = getStorage(app);
+
+
+export  {app, db, collection, getDocs, addDoc,doc, setDoc, getDoc, updateDoc, imageDB, uploadBytes, ref, query, limit, orderBy, serverTimestamp};
